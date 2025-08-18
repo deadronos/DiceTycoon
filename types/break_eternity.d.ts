0b41395cfe33a8f28329a4d939e99eabf9b925f3
Lars <iridanblub@hotmail.de>
2025-08-18 08:57:36 +0200

    test(storage): add storage unit tests; types: strengthen break_eternity declarations

--- types/break_eternity.d.ts
+++ types/break_eternity.d.ts
@@ -1,4 +1,22 @@
 declare module '@patashu/break_eternity.js' {
-  const Decimal: any;
+  interface DecimalInstance {
+    toString(): string;
+    toNumber(): number;
+    add(n: any): DecimalInstance;
+    plus(n: any): DecimalInstance;
+    sub(n: any): DecimalInstance;
+    times(n: any): DecimalInstance;
+    gte(n: any): boolean;
+    lt(n: any): boolean;
+    pow(n: any): DecimalInstance;
+  }
+
+  interface DecimalConstructor {
+    new (value?: number | string): DecimalInstance;
+    (value?: number | string): DecimalInstance;
+    fromNumber?(n: number): DecimalInstance;
+  }
+
+  const Decimal: DecimalConstructor;
   export default Decimal;
 }
